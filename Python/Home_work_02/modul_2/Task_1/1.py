9afe0fa72e0feec36f198ef9961faba82ab99488
Gor8ts <krotov1989sergey@gmail.com>
2023-04-11 14:30:43 +0300

    Home work

---
+++ Python/Home_work_02/modul_2/Task_1/1.py
@@ -0,0 +1,11 @@
+x = int (input ('Please enter any number:'))
+y = int (input ('Please enter second any number:'))
+z = int (input ('Please enter third any number:'))
+print ('Choose what to do with numbers, add or multiply')
+print ('1-add')
+print ('2-multiply')
+choise = int (input ('Please enter number your choise:'))
+if choise == 1:
+    print (x+y+z,'the sum of your numbers')
+else:
+    print (x*y*z,'product of your numbers')
