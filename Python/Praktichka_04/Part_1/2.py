4da7ada98e27fcbd5f303c0ee50fbfbcaeb0a70d
Gor8ts <krotov1989sergey@gmail.com>
2023-05-08 17:08:18 +0300

    praktichka part 1(1,2,3)done

--- Python/Praktichka_04/Part_1/2.py
+++ Python/Praktichka_04/Part_1/2.py
@@ -1,1 +1,18 @@
-# Пользователь вводит с клавиатуры строку. Посчитайте количество букв, цифр в строке. Выведите оба количества на экран.
+# Пользователь вводит с клавиатуры строку. Посчитайте количество букв, цифр в строке. 
+# Выведите оба количества на экран.
+
+
+string = input ('enter string:')
+sum_number = 0
+sum_letters = 0
+sum_lettersA =0
+for i in string:
+    if '0' <= i <= "9":
+        sum_number +=1
+    if 'a' <= i <='z':
+        sum_letters +=1
+    if 'A' <= i <= 'Z':
+        sum_lettersA +=1
+
+print('Quantity number =',sum_number)
+print ('Quantity letters =',sum_letters + sum_lettersA)
