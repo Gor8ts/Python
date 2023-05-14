ba5a8a3db868bde74de02bdf09d90f04123b9736
Gor8ts <krotov1989sergey@gmail.com>
2023-05-14 19:24:40 +0300

    Paktichka and home work 04 Part_2 and 3 done

--- Python/Home_work_04/Part_2/1.py
+++ Python/Home_work_04/Part_2/1.py
@@ -4,4 +4,33 @@
 # В нашем примере это 35. Арифметическое выражение
 # может состоять только из трёх частей: число, операция,
 # число. Возможные операции: +, -,*,/
-
+while True:
+    print('press 0 to stop the program')
+    example = input ('enter example: ')#example - пример
+    if example != '0':
+        for i in example:
+            if i == '+':
+                symbol = '+'
+                res = example.split('+')
+            if i == '-':
+                symbol = '-'
+                res = example.split('-')
+            if i == '/':
+                symbol = '/'
+                res = example.split('/')
+            if i == '*':
+                symbol = '*'
+                res = example.split('*')
+#Для  швидкої перевірки зроблено в циклі
+        if symbol == '+':
+            resvariabl = int(res[0]) + int(res[1])
+        elif symbol == '-':
+            resvariabl = int(res[0]) - int(res[1])
+        elif symbol == '/':
+            resvariabl = int(res[0]) / int(res[1])
+        elif symbol == '*':
+            resvariabl = int(res[0]) * int(res[1])
+        print (resvariabl)
+    else:
+        break
+    
--- Python/Praktichka_04/Part_2/1.py
+++ Python/Praktichka_04/Part_2/1.py
@@ -5,19 +5,29 @@
 # ■ Посчитайте количество восклицательных знаков в
 # тексте.
 
-
-
-
-
 lst = input ('enter text:')
-lst1 = lst.capitalize()
-print (lst1)
-
+sign = 0
 lst2 = 0
+lst3 = 0
+
+sentences = lst.split ('. ')
+format_lst = ""
+
 for i in lst:
     if '0' <= i <= '9':
         lst2 += 1
-    
-print (lst2)
+    if i == ',' or i == '.' or i == ':' or i == ';':
+        lst3 +=1
+    if i == '!':
+        sign +=1
 
-lst3 = 0
+for sentence in sentences:
+    format_lst += sentence.capitalize() + ". "
+format_lst = format_lst[:-2]
+
+print (format_lst)
+
+print ('numbers =',lst2)
+print ('signs =',lst3)
+print ('sign ! =',sign)
+    
