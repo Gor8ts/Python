740fa88573a6b2be88bef5a8d6219c2a2b81df38
Gor8ts <krotov1989sergey@gmail.com>
2023-05-15 20:25:40 +0300

    Create praktichka part 2-3 home work part 2-3

---
+++ Python/Praktichka_05/Part_2/1.py
@@ -0,0 +1,13 @@
+# Напишите функцию, вычисляющую сумму элементов списка целых. Список передается вкачестве параметра.
+from random import random
+
+number = int (input('enter quatity number for audit:'))
+lst0 = []
+for i in range (number):
+    lst0.append(random())
+sum_numbers = sum(lst0)
+print (lst0)
+
+def sum_numbers():
+    return sum_numbers
+print (sum_numbers)
