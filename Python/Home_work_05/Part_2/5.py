49b3061e28cb01186510ba676aa8354d1abfa804
Gor8ts <krotov1989sergey@gmail.com>
2023-05-29 16:39:27 +0300

    did not full done Part_3 home and class work

--- Python/Home_work_05/Part_2/5.py
+++ Python/Home_work_05/Part_2/5.py
@@ -1,3 +1,12 @@
 # Напишите функцию, которая получает два списка в
 # качестве параметра и возвращает список, содержащий
 # элементы обоих списков.
+
+def list_concatenation (a,b):
+    lst1 = list(a)
+    lst2 = list(b)
+    lst1.extend(lst2)
+    return lst1
+
+print (list_concatenation([21,2,414,1,'cdc',1,],[3,5,2,'fsl',3,'3',]))
+
--- Python/Praktichka_05/Part_3/3.py
+++ Python/Praktichka_05/Part_3/3.py
@@ -1,2 +1,17 @@
 # Написать рекурсивную функцию, которая выводит N
 # звезд в ряд, число N задает пользователь. Проиллюстрируйте работу функции примером.
+
+def picture_recursiv(n):
+    if n == 0:
+        return
+    else:
+        if n < 0:
+            print ('*', end = '')
+            picture_recursiv (n+1)
+        else:
+            print ('*', end = '')
+            picture_recursiv(n-1)
+    
+a = int (input('enter number for picture:'))
+
+print (picture_recursiv(a))
