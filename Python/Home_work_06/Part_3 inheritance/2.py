2f6dfa0d7308ee5f7655d327c13a6d4b45a4e43d
Gor8ts <krotov1989sergey@gmail.com>
2023-06-05 11:07:33 +0300

    home work-06 part 3 and praktichka 06 part 2 inheritance done

--- Python/Home_work_06/Part_3 inheritance/2.py
+++ Python/Home_work_06/Part_3 inheritance/2.py
@@ -6,3 +6,50 @@
 # Cruiser (содержит информацию о крейсере).
 # Каждый из классов должен содержать необходимые 
 # для работы методы.
+
+class Ship():
+    def __init__(self,size,displacement,design,materials,armament,propulsion,protection_sistems) -> None:
+        self.size = size
+        self.displacement = displacement
+        self.design = design
+        self.materials = materials
+        self.armament = armament
+        self.propulsion = propulsion
+        self.protection_sistems = protection_sistems
+         
+class Frigate(Ship):
+    def __init__(self, size = 0, displacement = 0, design = 0, materials = 0, armament = 0, \
+                 propulsion = 0, protection_sistems = 0) -> None:
+        super().__init__(size, displacement, design, materials, armament, propulsion,\
+                          protection_sistems)
+
+    def enemy_detection(self):
+        print ('Detection enemy. Put weapons on alert')
+        return self.weapons()
+    def weapons(self):
+        print ('Weapons are being prepared for use')
+
+class Destroyer(Ship):
+    def __init__(self, size = 0, displacement = 0, design = 0, materials = 0, armament = 0,\
+                  propulsion = 0, protection_sistems = 0) -> None:
+        super().__init__(size, displacement, design, materials, armament, propulsion, \
+                         protection_sistems)
+    
+    def firing_practice(self):
+        print ('Practice firing is underway, unauthorized persons are requested to leave the shooting site')
+
+class Cruiser(Ship):
+    def __init__(self, size = 0, displacement = 0, design = 0, materials = 0, armament = 0,\
+                  propulsion = 0, protection_sistems = 0) -> None:
+        super().__init__(size, displacement, design, materials, armament, propulsion,\
+                          protection_sistems)
+
+    def training_maneuvers(self):
+        print('A large cruiser is less maneuverable due to it\'s sizebut does it\'s job')
+
+frigat = Frigate()
+print (frigat.enemy_detection())
+destroyer = Destroyer()
+print (destroyer.firing_practice())
+cruiser = Cruiser()
+print (cruiser.training_maneuvers())
--- Python/Home_work_06/Part_3 inheritance/3.py
+++ Python/Home_work_06/Part_3 inheritance/3.py
@@ -5,7 +5,18 @@
 # Реализовать методы для вывода суммы на экран, задания значений для частей
 
 class Money():
-    def __init__(self,hrivna,copiyci) -> None:
-        self.hrivna = hrivna
-        self.copiyci = copiyci
-        return (self.hrivna),(self.copiyci)
+    def __init__(self,whole_part,fractional_part) -> None:
+        self.whole_part = whole_part
+        self.fractional_part = fractional_part
+    
+    def display_amount (self):
+        print(f'{self.whole_part}.{self.fractional_part}')
+
+    def set_amount (self,whole_part, fractional_part):
+        self.whole_part = whole_part
+        self.fractional_part = fractional_part
+
+hryvni = Money(25,50)
+hryvni.display_amount()
+hryvni.set_amount(30,32)
+hryvni.display_amount()
